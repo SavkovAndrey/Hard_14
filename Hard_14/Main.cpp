a12f542ec2f278ae0f7bcb1bb61c4a407494880b
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-11-29 20:03:47 +0300

    get, set

--- Hard_14/Main.cpp
+++ Hard_14/Main.cpp
@@ -3,17 +3,52 @@
 
 using namespace std;
 
-class Product
+class Product                  // базовый для всех класс
 {
 public:
 	Product();
 	~Product();
+	void SetName(string);
+	void SetPrice(int);
+	void SetWeight(int);
+	string GetName();
+	int GetPrice();
+	int GetWeight();
 
 private:
-	string name;
-	int price;
-	int weight;
+	string name;               // название
+	int price = 0;             // цена
+	int weight = 0;            // вес
 };
+
+class Buy                      // производный для Product, базовый для Check
+{
+public:
+	Buy();
+	~Buy();
+	void SetPiece(int);
+	void SetTotal_price(int);
+	void SetTotal_Weight(int);
+	int GetPiece();
+	int GetTotal_Price();
+	int GetTotal_Weight();
+
+private:
+	int piece = 0;              // колличество в штуках
+	int total_price = 0;        // общая цена
+	int total_weight = 0;       // общий вес
+};
+
+class Check                     // Выводит инфу о товаре и покупке (производный от Bay)
+{
+public:
+	Check();
+	~Check();
+
+private:
+};
+
+
 
 
 
@@ -29,11 +64,91 @@ int main()
 	return 0;
 }
 
+//-------------------------------------------- Product
 
 Product::Product()
 {
 }
-
 Product::~Product()
+{
+}
+ 
+//--------------------------------------------Product::Get::Set
+void Product::SetName(string name)
+{
+	this->name = name;
+}
+
+void Product::SetPrice(int price)
+{
+	this->price = price;
+}
+
+void Product::SetWeight(int weight)
+{
+	this->weight = weight;
+}
+
+string Product::GetName()
+{
+	return name;
+}
+
+int Product::GetPrice()
+{
+	return price;
+}
+
+int Product::GetWeight()
+{
+	return weight;
+}
+
+//---------------------------------------------- Buy
+
+Buy::Buy()
+{
+}
+Buy::~Buy()
+{
+}
+
+//---------------------------------------------- Buy::Get::Set
+void Buy::SetPiece(int piece)
+{
+	this->piece = piece;
+}
+
+void Buy::SetTotal_price(int total_price)
+{
+	this->total_price = total_price;
+}
+
+void Buy::SetTotal_Weight(int total_weight)
+{
+	this->total_weight = total_weight;
+}
+
+int Buy::GetPiece()
+{
+	return piece;
+}
+
+int Buy::GetTotal_Price()
+{
+	return total_price;
+}
+
+int Buy::GetTotal_Weight()
+{
+	return total_weight;
+}
+
+//---------------------------------------------- Check
+
+Check::Check()
+{
+}
+Check::~Check()
 {
 }
